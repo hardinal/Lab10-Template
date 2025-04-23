4045727616bc23e6cd1c7163f453aa13e3c96263
hardinal <hardinal@appstate.edu>
2025-04-23 16:19:15 -0400

    c1

--- src/main/maze/MazeSolver.java
+++ src/main/maze/MazeSolver.java
@@ -9,7 +9,8 @@ import java.util.Scanner;
  * 
  * @author Mitch Parry
  * @author Willow Sapphire
- * @version 11/02/2023
+ * @author Austin Hardin
+ * @version 23APR25
  */
 public class MazeSolver
 {
@@ -30,6 +31,11 @@ public class MazeSolver
     private boolean[][] wall;
 
     /**
+     * The game map, as a 2D array of booleans.
+     * True indicates the spot has been visited.
+     */
+    private boolean[][] visited;
+    /**
      * Constructor sets up the maps and the path list.
      * 
      * @param mazeFile name of the file containing the map.
@@ -37,6 +43,7 @@ public class MazeSolver
     public MazeSolver(String mazeFile)
     {
         loadMaze(mazeFile);
+        visited = new boolean[HEIGHT][WIDTH];
     }
 
     /**
@@ -91,5 +98,74 @@ public class MazeSolver
             System.out.println();
         }
         System.out.println();
+    }
+    /**
+     * Calls the recursive search.
+     *
+     * @return a string of moves to reach the goal, null if no path.
+     */
+    public String findSolution()
+    {
+        return backtrack(0, 0, "");
+    }
+
+    /**
+     * Checks if the cell bounds, !wall, and !visited.
+     *
+     * @param row current row
+     * @param col current column
+     * @return true if valid
+     */
+    private boolean isValid(int row, int col)
+    {
+        return row >= 0 && row < HEIGHT
+                && col >= 0 && col < WIDTH
+                && !wall[row][col]
+                && !visited[row][col];
+    }
+
+
+    /**
+     * Backtrack recursion excursion method to find a path.
+     *
+     * @param row   current row
+     * @param col   current column
+     * @param path  path built so far (e.g. "down right ...")
+     * @return full path string if solution found; null otherwise
+     */
+    private String backtrack(int row, int col, String path)
+    {
+        if (row == HEIGHT - 1 && col == WIDTH -1)
+        {
+            return path;
+        }
+
+        if (!isValid(row, col))
+        {
+            return null;
+        }
+
+        visited[row][col] = true;
+
+        String result;
+
+        // GOING UP
+        result = backtrack(row - 1, col, path + "up ");
+        if (result != null) return result;
+
+        // GET DOWN GET DOWN
+        result = backtrack(row + 1, col, path + "down ");
+        if (result != null) return result;
+
+        // SLIIIIDE TO THE LEFT
+        result = backtrack(row, col - 1, path + "left ");
+        if (result != null) return result;
+
+        // SLIIIIDE TO THE RIGHT
+        result = backtrack(row, col + 1, path + "right ");
+        if (result != null) return result;
+
+        //LOST
+        return null;
     }
 }
